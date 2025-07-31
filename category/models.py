a3fe65f388d9fcec1b0292c90c63c79c9629911e
Alamdar1 <alamdarali.900@gmail.com>
2025-07-30 21:00:26 -0400

    Category model created

---
+++ category/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+
+class Category(models.Model):
+    category_name=models.CharField(max_length=60)
+    slug=models.CharField(max_length=60)
+    description=models.TextField(max_length=120,blank=True)
+    cat_image=models.ImageField(upload_to="images/",blank=True)
+    
+    def __str__(self):
+     return self.category_name
+     
